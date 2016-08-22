828e29c53c4f9e1d3c1d3d57608b39577771dfae
mm86 <madhuvanmahesh@gmail.com>
2016-08-21 20:08:26 -0700

    fix: Get first and last name from user and display full name

--- Variables/name.rb
+++ Variables/name.rb
@@ -1,4 +1,5 @@
-name = gets.chomp;
-10.times do
-  puts name
-end
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Your full name is " + first_name + " " + last_name
