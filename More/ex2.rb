3b76465b46469b13f982e141f45a6074c726d5eb
mm86 <madhuvanmahesh@gmail.com>
2016-08-23 16:56:47 -0700

    fix: Add .call method to block

--- More/ex2.rb
+++ More/ex2.rb
@@ -1,2 +1,8 @@
  #<Proc:0x0055f320b5e230@(repl):5>
  # a proc object is returned as .call method is not used.
+
+def execute(&block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }
